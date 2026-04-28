67ea46cf0e8fd40f2163fee5501e7de0040f7f37
Sanjoy Dey <sanjaydey21@yahoo.com>
2026-04-28 11:12:12 +0600

    Revert "change index file for test purpose"
    This reverts commit 92ee38cdce24527e1138a908821d58c9db8f7792.

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-// $loginpage = 1;
-// include "mysql.php";
+$loginpage = 1;
+include "mysql.php";
 
-// if (isset($session_id) && $session_id) {
-//     $row = db_query_first( "select userid, redirectURL from user where id = $session_id " );
-//     $redirectURL = ($row["redirectURL"]) ? $row["redirectURL"] : "/login.php";
-//     header( "Location: $redirectURL" );
-//     exit; // Important to exit after header
-// }
-// else
-// {
-//  $redirectURL = ($row["redirectURL"]) ? $row["redirectURL"] : "/login.php";
-//     header( "Location: $redirectURL" );
-//  exit; // Important to exit after header
-// }
-echo "<h1 align='center'>Welcome to DOE</h1>";
+if (isset($session_id) && $session_id) {
+    $row = db_query_first( "select userid, redirectURL from user where id = $session_id " );
+    $redirectURL = ($row["redirectURL"]) ? $row["redirectURL"] : "/login.php";
+    header( "Location: $redirectURL" );
+    exit; // Important to exit after header
+}
+else
+{
+ $redirectURL = ($row["redirectURL"]) ? $row["redirectURL"] : "/login.php";
+    header( "Location: $redirectURL" );
+ exit; // Important to exit after header
+}
+
 ?>
